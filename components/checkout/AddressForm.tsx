54a6189979aa7cc61d146f201a9602cc0e577b1c
mesrarM <mesrar.mohamed@gmail.com>
2022-05-06 16:28:24 +0200

    update some file

--- components/checkout/AddressForm.tsx
+++ components/checkout/AddressForm.tsx
@@ -40,7 +40,7 @@ export function AddressForm({
       lastName: existingAddressData?.lastName || "",
       phone: existingAddressData?.phone || "",
       country: "AE",
-      countryArea: "dxb",
+      countryArea: "DU",
       streetAddress1: existingAddressData?.streetAddress1 || "",
       city: existingAddressData?.city || "",
       postalCode: existingAddressData?.postalCode || "",
